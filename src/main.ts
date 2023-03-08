34b46df37cb64c6ee020d4310f099f1e1e9bc307
so1ve <nn_201312@163.com>
2023-03-08 13:29:17 +0800

    feat: key

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import "./style.css";
 
-const idEl = document.getElementById("id") as HTMLInputElement;
+const keyEl = document.getElementById("key") as HTMLInputElement;
 const imageEl = document.getElementById("image") as HTMLInputElement;
 const colorEl = document.getElementById("color") as HTMLSelectElement;
 const submitEl = document.getElementById("submit") as HTMLButtonElement;
 const resultEl = document.getElementById("result") as HTMLPreElement;
+
+const query = new URLSearchParams(window.location.search);
+let key = query.get("key");
+const hasKeyAsQuery = !!key;
+keyEl.value = key || "";
+
+keyEl.disabled = hasKeyAsQuery;
+
+keyEl.addEventListener("input", () => key = keyEl.value);
 
 const copy = async () => {
   const courseName: string[][] = [[]];
@@ -18,32 +27,24 @@ const copy = async () => {
     }
     courseName.push(current);
   }
-
-  const orderId = idEl.value;
-  const idIsNumber = !Number.isNaN(Number(orderId));
   const formData = new FormData();
   const image = imageEl.files?.[0];
   if (image) {
     formData.append("file", image);
   }
-  if (orderId.length !== 4) {
-    resultEl.innerText = "订单号为四位数字";
-    return;
+  let photoFilename: string | undefined;
+  if (image) {
+    const imageResp = await fetch(`${import.meta.env.VITE_API_URL}upload_image?key=${key}`, {
+      method: "POST",
+      body: formData,
+    }).then(r => r.json());
+    photoFilename = imageResp.filename;
   }
-  if (!idIsNumber) {
-    resultEl.innerText = "订单号必须为数字";
-    return;
-  }
-  const imageResp = await fetch("http://192.168.123.135:8000/upload_image", {
-    method: "POST",
-    body: formData,
-  }).then(r => r.json());
-  const photoFilename = imageResp.filename;
   const color = colorEl.value;
   const resp = await fetch(`${import.meta.env.VITE_API_URL}schedule_submit`, {
     method: "POST",
     body: JSON.stringify({
-      order_id: orderId,
+      key,
       color,
       course_name: courseName,
       photo_filename: photoFilename,
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from "vite";
+import PluginLegacy from "@vitejs/plugin-legacy";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    // Must transform esm
+    PluginLegacy({
+      targets: ["defaults", "not IE 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
+});
