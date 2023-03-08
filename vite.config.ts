30f884795ffd8468e5424d6fdd1ebd0d65bb096d
so1ve <nn_201312@163.com>
2023-03-08 13:32:34 +0800

    feat: legacy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import PluginLegacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
-    // Must transform esm
     PluginLegacy({
-      targets: ["defaults", "not IE 11"],
+      targets: ["defaults", "IE 11"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
     }),
   ],
